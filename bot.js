38e18b871ea4222b18b48a646dd79cb2dd040fbf
popuguy <d3xrow@gmail.com>
2020-03-14 01:21:17 +0000

    refactor: edit entry point

--- bot.js
+++ bot.js
@@ -1,40 +1,32 @@
-const Telegraf = require('telegraf')
-const Telegram = require('telegraf/telegram')
-const { resources, upload } = require('ya-disk')
-const { promisify } = require('util');
+const Telegraf = require("telegraf");
+const Extra = require("telegraf/extra");
+const Markup = require("telegraf/markup");
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-const telegram = new Telegram(process.env.BOT_TOKEN)
+const session = require("telegraf/session");
 
-bot.use((ctx) => {
-  console.log(ctx.message)
-})
+const { resources, upload, meta } = require("ya-disk");
 
-bot.catch((err, ctx) => {
-  console.log(`Ooops, encountered an error for ${ctx.updateType}`, err)
-})
-
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.on(['video', 'document'], async (ctx) => {
-    let fileId;
-    let filePath;
-    let fileName;
-    const username = ctx.from.username
-    await resources.create(process.env.YA_TOKEN, username)
-    if('video' in ctx.message) {
-        fileId = ctx.message.video.file_id
-        fileName = ctx.message.video.file_unique_id
-        filePath = await telegram.getFileLink(fileId)
-    }
-    if('document' in ctx.message) {
-        fileId = ctx.message.document.file_id
-        fileName = ctx.message.document.file_name
-        filePath = await telegram.getFileLink(fileId)
-    }
-    upload.remoteFile(process.env.YA_TOKEN, filePath, `disk:/${username}/${fileName}.mp4`)
-
-})
+// сцены
+const { stage } = require("./src/scenes");
 
 
-bot.launch()
-console.log('bot listen')
+const botToken = process.env.BOT_TOKEN;
+const bot = new Telegraf(botToken);
+
+
+//bot.use(ctx => {
+//  console.log(ctx.message);
+// });
+
+//bot.catch((err, ctx) => {
+//console.log(`Ooops, encountered an error for ${ctx.updateType}`, err);
+//});
+
+bot.use(session());
+bot.use(stage.middleware());
+
+
+bot.command("start", ctx => ctx.scene.enter('greeter'));
+
+bot.launch();
+console.log("bot listen");
